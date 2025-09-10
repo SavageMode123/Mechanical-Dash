5bbe22d96f16bc9d7627d2bae35edee4336874b3
TheWhiteLionGod <aaryan12jul@gmail.com>
2025-09-10 13:27:04 -0400

    Made Menu Appear When Game Ends
    Added Game End Signal
    Stopped Player Physics when Game Ends
    Made Menu Visible when Game Ends

--- Events.gd
+++ Events.gd
@@ -1,3 +1,4 @@
 extends Node
 
 signal startGame
+signal endGame
--- GUI.gd
+++ GUI.gd
@@ -4,6 +4,12 @@ extends Control
 @onready var playButton: Button = $"Menu/TextureRect/HBoxContainer/PlayButton"
 @onready var Menu: Control = $"Menu"
 
+func _ready() -> void:
+	Events.endGame.connect(gameEnd)
+
 func _on_play_button_pressed() -> void:
 	Events.startGame.emit()
 	Menu.visible = false
+
+func gameEnd() -> void:
+	Menu.visible = true
