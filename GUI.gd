14230c6c6d9629585900f459c2a41bb822f48411
TheWhiteLionGod <aaryan12jul@gmail.com>
2025-09-10 13:18:50 -0400

    Added Menu Background
    Added Menu.png to Images Dir
    Added Menu Texture as parent of HBoxContainer
    Made Menu Invisible on Game Start

--- GUI.gd
+++ GUI.gd
@@ -1,9 +1,9 @@
 extends Control
 
 @export var Events: Node
-@onready var playButton: Button = $"Menu/HBoxContainer/PlayButton"
+@onready var playButton: Button = $"Menu/TextureRect/HBoxContainer/PlayButton"
+@onready var Menu: Control = $"Menu"
 
 func _on_play_button_pressed() -> void:
 	Events.startGame.emit()
-	playButton.visible = false
-
+	Menu.visible = false
